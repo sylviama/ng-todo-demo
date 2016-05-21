d33d19377393e5abe9e2aa056fd692e7ded9b305
Sylvia Ma <xiangwma@umich.edu>
2016-05-21 13:31:55 -0500

    route

--- app/app.js
+++ app/app.js
@@ -1,2 +1,18 @@
-console.log("hello");
+var app= angular.module("TodoApp",["ngRoute"]);
 
+app.config(function($routeProvider){
+  $routeProvider.
+    when("/items/list",{
+      templateUrl:"partials/item-list.html",
+      controller:"TodoCtrl"
+    }).
+    when("/items/new",{
+      templateUrl:"partials/item-new.html",
+      controller:"TodoCtrl"
+    }).
+    when("/items/details",{
+      templateUrl:"partials/item-details.html",
+      controller:"TodoCtrl"
+    }).
+    otherwise("/items/list");
+})
