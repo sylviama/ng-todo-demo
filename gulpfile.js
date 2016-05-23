dab22f4acf145c27502ca7857f7e853182b828dd
Sylvia Ma <xiangwma@umich.edu>
2016-05-23 16:43:01 -0500

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,11 @@ var sass = require('gulp-sass');
 gulp.task('default', ['sassify', 'watch']);
 
 gulp.task('watch', function() {
-  gulp.watch('./sass/**/*.scss', ['sassify']);
+  gulp.watch('../sass/**/*.scss', ['sassify']);
 });
 
 gulp.task('sassify', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('../sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('../css'));
 });
